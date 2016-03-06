531829a2496c57d36ba363df0a702feea95bb98e
nexus <github.com@cyberstalker.eu>
2016-03-06 15:16:28 +0100

    * remove outdated configuration options that are not needed for yalec.py

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 BASE="https://acme-staging.api.letsencrypt.org/directory"
-KEY="certs/user-privkey.pem"
-CSR="certs/csr.pem"
 TERMS="https://letsencrypt.org/documents/LE-SA-v1.0.1-July-27-2015.pdf"
-EMAIL="test@example.com"
-DOMAIN="example.com"
-WEBDIR="/var/www/acme"
+WEBDIR="/tmp/acme"
 CHALLENGE_PREFIX=".well-known/acme-challenge/"
 
 from letsencrypt import http
